9501bd3a13905e0807b50a9fec1eebf4f0c87572
Hakim Malkawi <hakim.malkawi@gmail.com>
2022-09-11 09:58:24 +0300

    IE 11 Not Fun

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
+import "react-app-polyfill/ie11"
+import "react-app-polyfill/stable"
+import "./polyfills"
 import React, { StrictMode } from "react"
 import { render } from "react-dom"
 import App from "./App"
---
+++ src/polyfills.js
@@ -0,0 +1,2 @@
+if (!window.crypto) window.crypto = window.msCrypto
+// This is for the nanoid package that I use to set keys when mapping elements
